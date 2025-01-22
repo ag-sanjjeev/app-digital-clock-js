4e29d31209fd75ea6006f6da9d8356d46e0b946b
ag-sanjjeev <91313054+ag-sanjjeev@users.noreply.github.com>
2025-01-22 13:06:14 +0530

    Updated Script

--- scripts/main.js
+++ scripts/main.js
@@ -18,4 +18,37 @@
 
 window.addEventListener('load', function() {
 	console.log('loaded');
+	var cObject = new Clock();
+	cObject.run();
 });
+
+// Clock function
+function Clock() {
+	var self = this;
+
+	self.init = function() {
+		// DOM reference
+		self.hours = document.getElementById('hours-text');
+		self.minutes = document.getElementById('minutes-text');
+		self.seconds = document.getElementById('seconds-text');
+		self.meridian = document.getElementById('meridian-text');
+		self.hoursDot = document.querySelector('.hours .dot');
+		self.minutesDot = document.querySelector('.minutes .dot');
+		self.secondsDot = document.querySelector('.seconds .dot');
+		self.meridianDot = document.querySelector('.meridian .dot');
+		self.hoursCircle = document.getElementById('hours-circle');
+		self.minutesCircle = document.getElementById('minutes-circle');
+		self.secondsCircle = document.getElementById('seconds-circle');
+		self.meridianCircle = document.getElementById('meridian-circle');
+	};
+
+	self.run = function() {
+		self.init();
+		self.interval = setInterval(function() { self.evaluate(); }, 1000);
+	};
+
+	self.evaluate = function() {
+		console.log('evaluate method invoked');
+	};
+}
+
